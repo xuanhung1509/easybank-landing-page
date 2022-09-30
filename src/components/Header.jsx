42fdce4e5b6ecd03f0bdf30feb6cb2c64deb6eb2
xuanhung1509 <vuxuanhung15092002@gmail.com>
2022-09-30 22:46:04 +0700

    feat: create header on desktop

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,32 @@
 import React from 'react';
+import { Button } from './shared';
+import logo from '../assets/logo.svg';
+
+const navItems = ['Home', 'About', 'Contact', 'Blog', 'Careers'];
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header className='py-3'>
+      <div className='container'>
+        <div className='flex items-center justify-between gap-2'>
+          <div>
+            <img src={logo} alt='logo' />
+          </div>
+          <ul className='flex items-center justify-center gap-4 '>
+            {navItems.map((item) => (
+              <li
+                key={item}
+                className='cursor-pointer text-eb-grayish-blue transition-colors hover:text-eb-dark-blue'
+              >
+                {item}
+              </li>
+            ))}
+          </ul>
+          <Button>Request Invite</Button>
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
