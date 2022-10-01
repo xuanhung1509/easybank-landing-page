35a3d67c1b08643f4a36edb2bdb69173e53a6793
xuanhung1509 <vuxuanhung15092002@gmail.com>
2022-10-01 23:03:06 +0700

    feat: create hero section on desktop

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,119 @@ function App() {
   return (
     <div>
       <Header />
-      {/* <Hero />
-      <Features />
+      <Hero />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta nam
+        dolore repudiandae porro nostrum aliquam quas nesciunt consequuntur
+        minus a culpa velit perspiciatis modi itaque incidunt alias ullam ut,
+        vero molestias unde. Beatae sunt et aliquid blanditiis a provident
+        accusamus distinctio nobis repellat natus unde ex aliquam delectus,
+        doloribus voluptatibus quia expedita itaque tempora! Sunt magnam quis,
+        est corrupti quam quas ipsa molestiae repudiandae dolore ipsum nobis
+        aliquid ducimus optio nostrum, sit, dolorem rerum consequatur
+        perspiciatis fugiat tempora minima consequuntur nesciunt! Quasi ex sequi
+        voluptatem nam eum porro iste, quibusdam dolor! Voluptatibus nesciunt
+        eligendi eum quisquam. Perspiciatis reprehenderit voluptate ullam. Sed
+        illo quaerat odio sapiente quia debitis deserunt cum dolore possimus
+        officia amet esse ducimus illum nulla, minima natus itaque consequuntur
+        corporis nam quod eaque commodi aliquid placeat fuga? Modi sapiente
+        delectus doloremque quibusdam atque alias eveniet? Ratione error ex
+        corporis, labore nam nisi aspernatur tenetur ducimus possimus
+        dignissimos unde, inventore nostrum esse neque cupiditate incidunt
+        commodi deleniti! Quod, saepe tenetur eum asperiores ea nihil corporis
+        ab libero porro obcaecati sunt, nam consectetur nemo earum quis
+        incidunt. Laboriosam quia excepturi, nesciunt obcaecati sequi maxime
+        molestiae sit facilis vel necessitatibus cupiditate! Iusto esse beatae
+        hic doloribus facere aspernatur explicabo voluptate, natus dolor animi
+        veniam dicta magni possimus nihil porro commodi sapiente. Omnis
+        exercitationem ullam suscipit, enim sint fuga aliquam impedit
+        consequatur doloribus magni libero repudiandae vero distinctio optio
+        cupiditate. Dolor nihil recusandae nostrum amet mollitia? Similique et
+        iure, in accusamus repudiandae quae mollitia eaque corporis sequi saepe
+        doloribus. Similique eius accusantium assumenda debitis consequuntur
+        facilis laudantium, neque ipsa enim hic, obcaecati animi corporis iusto,
+        esse voluptatum repellendus voluptates tenetur commodi aut asperiores?
+        Ab quis illo provident accusantium mollitia! Similique itaque doloremque
+        quas id animi, vel molestiae a rerum fuga fugit ipsum. Quaerat,
+        consequuntur modi iste impedit, ipsum esse amet facilis atque
+        voluptatibus ab ratione dignissimos debitis quis optio possimus. Debitis
+        dolor odit atque quis libero earum at. Totam deserunt fugiat velit
+        repellendus necessitatibus optio assumenda iste quaerat impedit sequi
+        obcaecati architecto odio doloribus minima aperiam iure ipsa, possimus
+        alias. Magni, voluptatibus voluptas molestias asperiores eligendi
+        voluptate obcaecati repudiandae qui ipsa! Modi praesentium aut quis
+        cupiditate earum. Assumenda consectetur doloremque quae voluptate
+        maiores saepe inventore pariatur facere, ad neque quam laudantium nemo
+        et sequi eaque commodi labore rerum dicta, error, quasi hic ut eum?
+        Tenetur, accusantium. Quia laboriosam molestias ipsam explicabo ratione,
+        fuga tempore velit aspernatur atque hic. Veritatis culpa laboriosam
+        soluta eius inventore cumque, neque molestiae corrupti impedit
+        distinctio, numquam fuga eum delectus dicta quod quis iure reiciendis,
+        vitae eligendi accusantium. Nisi odio soluta, voluptatibus quia dolorum
+        tenetur commodi totam laboriosam adipisci sapiente eligendi, error qui
+        minima consequuntur odit harum praesentium libero eos quasi! Assumenda,
+        ad? Totam repellendus aspernatur omnis tempora nobis. Eveniet quasi
+        maxime est, possimus qui eum autem voluptatibus eaque modi expedita.
+        Laborum earum doloribus similique harum maiores, commodi hic veritatis
+        deserunt magnam nemo dolor voluptatem ut animi facere. Animi esse id
+        perferendis ipsa labore officia aperiam sequi perspiciatis ut, porro
+        assumenda, et corrupti, possimus consequuntur omnis eligendi soluta
+        nobis. A odio facilis animi quisquam, quae in qui placeat alias ab quia
+        dignissimos nam itaque dicta enim provident iusto rerum facere suscipit
+        quis. Doloribus error maiores enim unde, sint beatae veniam accusamus ex
+        eveniet ullam quam provident alias nostrum soluta nemo deserunt quae
+        iure? Magni debitis tempore ratione, cum sapiente facilis exercitationem
+        expedita aliquid unde et, eligendi blanditiis magnam dignissimos dicta
+        rerum voluptatum ut, vero temporibus nulla? Voluptate beatae aspernatur,
+        veniam neque quam recusandae praesentium esse, consequuntur iusto
+        consequatur officiis ullam molestias quis. Perspiciatis consequatur
+        tenetur hic nesciunt eveniet ad quibusdam, itaque sequi quos laudantium
+        esse? Fugit, suscipit culpa praesentium eius debitis expedita eum minus
+        quaerat itaque error? Iste eos ea deleniti numquam veritatis, magni
+        ipsum et beatae obcaecati exercitationem? Ad voluptatibus reiciendis
+        sint unde iure doloribus voluptatem saepe assumenda vero explicabo,
+        praesentium amet maxime nemo similique molestiae blanditiis quibusdam
+        quo. Nesciunt deleniti, voluptatem eaque harum iusto quibusdam magnam
+        quos expedita, itaque natus commodi nostrum. Quos mollitia dolor velit
+        placeat error totam ducimus voluptates vitae quia illo quaerat magni
+        minima numquam nam autem cum dolore, ipsa facilis! Fugit, eaque. Neque
+        nobis et officiis cumque perferendis, ipsam cupiditate amet inventore
+        fugit voluptatibus quidem aspernatur placeat sit non tenetur aperiam
+        alias iusto quam impedit earum tempora ratione facilis explicabo odio?
+        Voluptatum enim incidunt odit harum? Nisi ex nemo ratione cum molestias
+        dolor nostrum cumque autem eligendi suscipit saepe ullam quod, quaerat
+        ea corrupti possimus amet accusantium porro ut totam debitis
+        dignissimos? Sapiente veniam unde, amet, soluta aut voluptatem quo
+        ratione voluptatum ipsum ullam eum. Facere cupiditate esse dicta,
+        assumenda dolore eum temporibus mollitia. Eos tempore iusto fuga
+        ducimus, deserunt quo architecto dignissimos illum possimus eligendi
+        modi earum ipsa, ab dolorem repellat eveniet asperiores natus reiciendis
+        molestiae debitis? Dolorem in dolore accusamus quod unde! Voluptate
+        magnam excepturi nostrum repellendus nobis minus deserunt, sit vel iusto
+        tempore at ipsam tempora possimus! Commodi, natus deleniti odio soluta
+        voluptatum quo consequuntur, maiores nihil temporibus eum molestiae? Ad
+        suscipit eum, molestias nulla reiciendis, similique soluta atque
+        sapiente modi voluptatem veritatis labore voluptatum minus totam alias
+        quasi quisquam minima incidunt! Praesentium repellendus consectetur
+        dolores, optio nobis ducimus doloremque fuga maiores deleniti quia saepe
+        unde facilis. Corrupti fuga non, illo nam voluptatem optio quasi, ab
+        suscipit aperiam alias beatae molestiae quos nihil assumenda? Eum
+        reiciendis quia laudantium. Magnam enim beatae asperiores reprehenderit
+        non eveniet tempore nulla distinctio error cupiditate id architecto ipsa
+        at reiciendis et, mollitia officia illum illo! Nesciunt vel eos voluptas
+        incidunt nihil, inventore reprehenderit a voluptatum illum eligendi
+        suscipit blanditiis repudiandae minus rerum pariatur iusto nam quas
+        explicabo quo dolor iure architecto? Quas voluptates dolorum, neque
+        distinctio quos expedita aliquam ab exercitationem maxime deleniti
+        delectus a nemo fuga voluptatem cumque quisquam voluptate odio sed
+        laudantium harum. Aspernatur quisquam quia assumenda veniam distinctio
+        expedita ipsum, possimus rerum sunt, mollitia accusamus soluta pariatur
+        commodi. Accusamus ipsa natus corrupti inventore ipsum consectetur
+        dolores, pariatur necessitatibus numquam illo animi, fuga vitae quos
+        aperiam iure? Voluptates, consequuntur est fugiat quam distinctio ab
+        voluptatem inventore, nemo qui sunt itaque esse porro.
+      </p>
+      {/* <Features />
       <Articles />
       <Footer /> */}
     </div>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,43 @@
 import React from 'react';
+import { Button } from './shared';
+import heroImage from '../assets/image-mockups.png';
+import bgIntroDesktop from '../assets/bg-intro-desktop.svg';
 
 function Hero() {
-  return <div>Hero</div>;
+  return (
+    <section className='relative flex min-h-[90vh] flex-col justify-center bg-eb-light-gray'>
+      <div className='container'>
+        <div className='flex items-center justify-between gap-8 overflow-hidden'>
+          <div className='flex w-[40%] flex-col items-start justify-center gap-6'>
+            <h1 className='text-5xl leading-tight text-eb-dark-blue'>
+              Next generation digital banking
+            </h1>
+            <p className='text-eb-grayish-blue'>
+              Take your financial life online. Your Easybank account will be a
+              one-stop-shop for spending, saving, budgeting, investing, and much
+              more.
+            </p>
+            <Button>Request Invite</Button>
+          </div>
+
+          <div className='absolute top-0 right-0 h-full w-full overflow-hidden'>
+            <img
+              src={bgIntroDesktop}
+              alt='intro background'
+              className='absolute -top-56 -right-96'
+            />
+          </div>
+          <div className='absolute top-0 right-0 h-[110vh] w-full overflow-hidden'>
+            <img
+              src={heroImage}
+              alt='phones'
+              className='absolute -top-28 -right-28'
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Hero;
