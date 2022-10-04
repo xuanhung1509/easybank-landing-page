79378f09c33138b0cf18c2a90ef49dad77f7a054
xuanhung1509 <vuxuanhung15092002@gmail.com>
2022-10-04 22:56:28 +0700

    style: add default button type, add hover style

--- src/components/shared/Button.jsx
+++ src/components/shared/Button.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 
 function Button({ children }) {
   return (
-    <button className='bg-eb-gradient rounded-full px-8 py-3 font-bold text-white'>
+    <button
+      type='button'
+      className='bg-eb-gradient rounded-full px-6 py-3 text-sm font-bold text-white transition-opacity hover:opacity-50'
+    >
       {children}
     </button>
   );
