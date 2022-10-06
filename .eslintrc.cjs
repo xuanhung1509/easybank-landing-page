c167d280a0e2a976fe5c0dc0aa27c0b3d224617e
xuanhung1509 <vuxuanhung15092002@gmail.com>
2022-10-06 22:13:53 +0700

    refactor: remove some ESLint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,6 +5,9 @@ module.exports = {
     'no-unused-vars': 'warn',
     'react/prop-types': 'off',
     'react/jsx-props-no-spreading': 'off',
+    'react/jsx-curly-brace-presence': 'warn',
     'import/no-extraneous-dependencies': 'off',
+    'jsx-a11y/click-events-have-key-events': 'off',
+    'jsx-a11y/no-static-element-interactions': 'off',
   },
 };
