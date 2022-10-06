b2569776aa09c32728f9be72323f0f1555593acf
xuanhung1509 <vuxuanhung15092002@gmail.com>
2022-10-06 23:08:22 +0700

    style: persist hero height on viewport change

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,10 +3,10 @@ import { Button } from './shared';
 
 function Hero() {
   return (
-    <section className='relative flex min-h-screen flex-col justify-center bg-eb-light-gray xl:min-h-[85vh]'>
+    <section className='hero relative flex flex-col justify-center bg-eb-light-gray'>
       <div className='container'>
-        <div className='relative top-36 flex flex-col items-center justify-center gap-6 px-4 text-center md:top-44 lg:top-0 lg:w-[40%] lg:items-start lg:px-0 lg:text-left tiny:top-0'>
-          <h1 className='text-3xl leading-tight text-eb-dark-blue md:text-5xl'>
+        <div className='relative top-28 flex flex-col items-center justify-center gap-6 px-4 text-center sm:top-56 lg:top-0 lg:w-[40%] lg:items-start lg:px-0 lg:text-left tiny:top-32'>
+          <h1 className='text-3xl leading-tight text-eb-dark-blue md:text-5xl tiny:text-2xl'>
             Next generation digital banking
           </h1>
           <p className='text-sm text-eb-grayish-blue md:text-base'>
@@ -20,7 +20,7 @@ function Hero() {
       <div className='absolute top-0 right-0 h-full w-full overflow-hidden'>
         <div className='image-background' />
       </div>
-      <div className='absolute top-0 right-0 h-screen w-full overflow-x-hidden'>
+      <div className='image-foreground-container absolute top-0 right-0 w-full overflow-x-hidden'>
         <div className='image-foreground' />
       </div>
     </section>
